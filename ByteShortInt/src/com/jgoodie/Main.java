3ac903108ca35676d757c1e4fd4e74281b31bfe7
jgoodie <john.goodman@gmail.com>
2018-06-15 11:19:46 -0700

    blah

---
+++ ByteShortInt/src/com/jgoodie/Main.java
@@ -0,0 +1,17 @@
+package com.jgoodie;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myVal = 10000;
+        int myMinVal = -2147483648; // smallest val for int
+        // int myMinVal = -5147483649; // too big!!
+        int myMaxVal = 2147483647; // largest val for int
+        //int myMaxVal = 5147483649; // too big!
+        byte myByteVal = 127; // -128 to 127
+        short myShortVal = -32768; // -32768 to 32767
+
+
+    }
+}
