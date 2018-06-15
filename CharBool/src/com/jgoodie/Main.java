d7207387dd69e54d5f433bb84b690246d91a022b
jgoodie <john.goodman@gmail.com>
2018-06-15 16:48:33 -0700

    init commit

--- CharBool/src/com/jgoodie/Main.java
+++ CharBool/src/com/jgoodie/Main.java
@@ -3,6 +3,15 @@ package com.jgoodie;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// chars and booleans
+        // char has width of 16 (2 bytes)
+        char myChar = 'j';
+        boolean myBool = true;
+        boolean isFemale = false;
+        if (isFemale) {
+            System.out.println("isFemale is True");
+        } else {
+            System.out.println("is not female");
+        }
     }
 }
