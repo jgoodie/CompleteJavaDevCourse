50f852f840d8a4929e97bbb81ba1dcc18218e1f3
jgoodie <john.goodman@gmail.com>
2018-06-15 10:36:39 -0700

    Hello World first check

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,16 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Bitches");
+        System.out.println("Hello, People!");
+        // Variables
+        double mynum = Math.pow(5,5) + (2*20);
+        int second_num = 12;
+        int third_num = 6;
+        double mytotal = mynum + second_num + third_num;
+        double sub1000 = mytotal - 1000;
+        System.out.println(mynum);
+        System.out.println(mytotal);
+        System.out.println(sub1000);
+
     }
 }
 
