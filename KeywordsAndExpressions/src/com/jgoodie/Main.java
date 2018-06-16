dd4895920f78bd3e2aa1df6e5ef28b606a27c581
jgoodie <john.goodman@gmail.com>
2018-06-16 10:08:49 -0700

    commit

--- KeywordsAndExpressions/src/com/jgoodie/Main.java
+++ KeywordsAndExpressions/src/com/jgoodie/Main.java
@@ -3,6 +3,14 @@ package com.jgoodie;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Keywords and expressions
+        // 1 mile is = to 1.609344 kilometers
+        double miles = 130.0;
+        double kilometers = (miles * 1.609344);
+        if (kilometers >= 200 && kilometers < 210) {
+            System.out.println("Congratulations on riding a double century!");
+        }
+
+
     }
 }
