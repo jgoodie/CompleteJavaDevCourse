b45e66ea65e6dd299238b7a3772a846f5f14492f
jgoodie <john.goodman@gmail.com>
2018-06-15 17:46:12 -0700

    commit

--- Operators/src/com/jgoodie/Main.java
+++ Operators/src/com/jgoodie/Main.java
@@ -3,6 +3,11 @@ package com.jgoodie;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int result = 1 + 2;
+        System.out.println(result);
+        int previousResult = result;
+        result = result*result;
+        System.out.println(result);
     }
 }
