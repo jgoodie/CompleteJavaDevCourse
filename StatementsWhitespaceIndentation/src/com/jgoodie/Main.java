22cd43d06bc67e63632db4ee3f8fd78ac8beb41c
jgoodie <john.goodman@gmail.com>
2018-06-16 10:29:15 -0700

    commit

---
+++ StatementsWhitespaceIndentation/src/com/jgoodie/Main.java
@@ -0,0 +1,22 @@
+package com.jgoodie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        double myVar = 2;
+        int counter = 0;
+        while (counter <= 10) {
+            System.out.println(counter +
+                    " " +
+                    myVar);
+            myVar = myVar * 2;
+            counter++;
+
+        }
+
+        int myVar2 = 10; int mvVar3 = 20; // can put statements on a single line
+
+        // you should know by now that indenting is just good manners
+    }
+}
