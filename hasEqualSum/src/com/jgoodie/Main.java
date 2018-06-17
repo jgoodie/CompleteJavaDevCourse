2a51a84090390a25d9d75ad38cc147fda21f8ec0
jgoodie <john.goodman@gmail.com>
2018-06-16 21:51:02 -0700

    commit

---
+++ hasEqualSum/src/com/jgoodie/Main.java
@@ -0,0 +1,16 @@
+package com.jgoodie;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static boolean hasEqualSum(int val1, int val2, int val3){
+        if ((val1 + val2) == val3){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
