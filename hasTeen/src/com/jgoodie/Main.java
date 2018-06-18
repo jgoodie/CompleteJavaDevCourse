7e8afa886f4861b2ad2e79b1eb4f54dcdddb4e58
jgoodie <john.goodman@gmail.com>
2018-06-18 09:54:43 -0700

    commit

--- hasTeen/src/com/jgoodie/Main.java
+++ hasTeen/src/com/jgoodie/Main.java
@@ -3,6 +3,23 @@ package com.jgoodie;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    public static boolean hasTeen(int val1, int val2, int val3){
+        if((val1 >= 13) && (val1 <= 19)){
+            return true;
+        }
+        else if((val2 >= 13) && (val2 <= 19)) {
+            return true;
+        }
+        else if((val3 >= 13) && (val3 <= 19)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
