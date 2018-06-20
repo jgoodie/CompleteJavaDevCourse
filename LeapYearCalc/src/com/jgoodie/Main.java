95fdab4dd53fbaba005d75a4c3450b79023b2cf4
jgoodie <john.goodman@gmail.com>
2018-06-19 19:07:14 -0700

    commit

--- LeapYearCalc/src/com/jgoodie/Main.java
+++ LeapYearCalc/src/com/jgoodie/Main.java
@@ -22,4 +22,56 @@ public class Main {
         }
         return rtVal;
     }
+    public static int getDaysInMonth(int month, int year) {
+        if((month < 1) || (month > 12)){
+            return -1;
+        }
+        if ((year < 1) || (year > 9999)){
+            return -1;
+        }
+        boolean leapYear = isLeapYear(year);
+        switch (month){
+            case 1:
+                return 31;
+                break;
+            case 2:
+                if (leapYear == true){
+                    return 29;
+                }
+                else {
+                    return 28;
+                }
+                break;
+            case 3:
+                return 31;
+                break;
+            case 4:
+                return 30;
+                break;
+            case 5:
+                return 31;
+                break;
+            case 6:
+                return 30;
+                break;
+            case 7:
+                return 31;
+                break;
+            case 8:
+                return 31;
+                break;
+            case 9:
+                return 30;
+                break;
+            case 10:
+                return 31;
+                break;
+            case 11:
+                return 30;
+                break;
+            case 12:
+                return 31;
+                break;
+        }
+    }
 }
