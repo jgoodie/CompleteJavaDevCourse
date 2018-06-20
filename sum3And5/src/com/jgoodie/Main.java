4f7cdaa57a780044e3d08a102c7221d98c669d41
jgoodie <john.goodman@gmail.com>
2018-06-20 10:39:09 -0700

    commit

---
+++ sum3And5/src/com/jgoodie/Main.java
@@ -0,0 +1,20 @@
+package com.jgoodie;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int sum = 0;
+	    int sumCount = 0;
+        for (int i = 1; i <= 1000; i++){
+            if((i%3 == 0) && (i%5 == 0)){
+                sum += i;
+                sumCount++;
+                System.out.println(i);
+            }
+            if (sumCount == 5){
+                break;
+            }
+        }
+
+    }
+}
