69469c633fa9a576e6f8f3d7fb15636ba4445d81
jgoodie <john.goodman@gmail.com>
2018-06-20 12:35:39 -0700

    commit

---
+++ isOdd/src/com/jgoodie/Main.java
@@ -0,0 +1,37 @@
+package com.jgoodie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1,100));
+        System.out.println(sumOdd(-1,100));
+        System.out.println(sumOdd(100,100));
+        System.out.println(sumOdd(100,-100));
+        System.out.println(sumOdd(100,1000));
+
+    }
+
+    public static boolean isOdd(int number){
+        if (number <= 0){
+            return false;
+        }
+        if (number%2 == 0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public static int sumOdd(int start, int end){
+        if ((end < start) || (start < 0) || (end < 0)){
+            return -1;
+        }
+        int oddCount = 0;
+        for (int i = start; i <= end; i++){
+           if(isOdd(i)){
+               oddCount += i;
+           }
+        }
+        return oddCount;
+    }
+}
