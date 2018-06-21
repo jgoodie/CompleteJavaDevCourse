c22e635593f8b98af069c70e637022a7854dcb3f
jgoodie <john.goodman@gmail.com>
2018-06-20 17:11:17 -0700

    commit

---
+++ sumDigits/src/com/jgoodie/Main.java
@@ -0,0 +1,24 @@
+package com.jgoodie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(1251));
+        System.out.println(sumDigits(2345));
+        System.out.println(sumDigits(10));
+    }
+
+    public static int sumDigits(int n){
+        if (n < 10){
+            return -1;
+        }
+        int length = String.valueOf(n).length();
+        int sum = 0;
+        for(int i = 1; i <= length; i++){
+            sum += n%10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
