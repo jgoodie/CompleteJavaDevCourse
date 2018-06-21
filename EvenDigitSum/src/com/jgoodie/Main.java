4231d79a72183263f4ecdc734b45b066412168ba
jgoodie <john.goodman@gmail.com>
2018-06-21 14:20:35 -0700

    commit

--- EvenDigitSum/src/com/jgoodie/Main.java
+++ EvenDigitSum/src/com/jgoodie/Main.java
@@ -3,20 +3,20 @@ package com.jgoodie;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
     }
 
-    public static boolean isEven(int number){
-        if (number <= 0){
-            return false;
-        }
-        if (number%2 == 0){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+//    public static boolean isEven(int number){
+//        if (number <= 0){
+//            return false;
+//        }
+//        if (number%2 == 0){
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+//    }
 
     public static int getEvenDigitSum(int number){
         if (number < 0){
@@ -25,7 +25,7 @@ public class Main {
         int length = String.valueOf(number).length();
         int evenSum = 0;
         for(int i = 1; i <=length; i++){
-            if(isEven(number%10)){
+            if(((number%10)%2) == 0){
                 evenSum += number%10;
             }
             number /= 10;
