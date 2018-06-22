00a8a9d6699ce694e860f8d11b324726f5ca39fa
jgoodie <john.goodman@gmail.com>
2018-06-21 21:46:15 -0700

    commit

--- NumberToWords/src/com/jgoodie/Main.java
+++ NumberToWords/src/com/jgoodie/Main.java
@@ -3,7 +3,7 @@ package com.jgoodie;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(reverse(-123));
+        numberToWords(-12);
     }
 
     public static int reverse(int number){
@@ -37,7 +37,48 @@ public class Main {
     }
 
     public static void numberToWords(int number){
+        if (number < 0){
+            System.out.println("Invalid Value");
+        }
+        int numLen = getDigitCount(number);
+        int revNum = reverse(number);
+        int forwardNum = 0;
+        for(int i=1; i<=numLen; i++){
+            //System.out.println(revNum%10);
+            switch (revNum%10){
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+            }
+            revNum /=10;
+        }
 
     }
-
 }
