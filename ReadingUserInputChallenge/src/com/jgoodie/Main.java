ba2f340bf826e4e356781a6a510f831c146c1d06
jgoodie <john.goodman@gmail.com>
2018-06-23 23:18:11 -0700

    commit

---
+++ ReadingUserInputChallenge/src/com/jgoodie/Main.java
@@ -0,0 +1,32 @@
+package com.jgoodie;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int inputCount = 1;
+	    int sum = 0;
+	    int number = 0;
+        boolean hasNextInt;
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Enter number #" + inputCount);
+            hasNextInt = scanner.hasNextInt();
+            if(hasNextInt){
+                number = scanner.nextInt();
+                inputCount++;
+                sum += number;
+                if(inputCount >= 11){
+                    break;
+                }
+            }
+            else{
+                System.out.println("Invalid Number");
+            }
+            scanner.nextLine(); // handle eol enter key
+        }
+        System.out.println("The sum is: " + sum);
+        scanner.close();
+    }
+}
