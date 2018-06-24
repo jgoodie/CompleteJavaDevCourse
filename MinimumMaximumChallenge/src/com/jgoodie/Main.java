2c445b341284a8b57f2d85f1d8f94e6facc094b9
jgoodie <john.goodman@gmail.com>
2018-06-24 00:03:04 -0700

    commit

---
+++ MinimumMaximumChallenge/src/com/jgoodie/Main.java
@@ -0,0 +1,38 @@
+package com.jgoodie;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int min = 0;
+	    int max = 0;
+        int number =0;
+        boolean hasNextInt;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter number: ");
+            hasNextInt = scanner.hasNextInt();
+            if(hasNextInt){
+                number = scanner.nextInt();
+                if((min == 0) && ( max == 0)){
+                    min = number;
+                    max = number;
+                }
+                if ((number >= max) && (number > min)){
+                    max = number;
+                }
+                if((number < max) && (number <= min) ){
+                    min = number;
+                }
+            }
+            else {
+                break;
+            }
+            scanner.nextLine();
+        }
+        System.out.println("min: " + min);
+        System.out.println("max: " + max);
+        scanner.close();
+    }
+}
